0d811ec651c93d320f17253c54ee7c1630ce6a83
ajay <ajayjarwal@gmail.com>
2020-01-25 09:02:22 +0530

    add validation funtction on registration form

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '526669b03620e2ed8bb3a81010d8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db  = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from flaskblog import db
 
