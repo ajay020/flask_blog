d3bec88943eca6ead574ce641cec2255131e945c
ajay <ajayjarwal@gmail.com>
2020-01-26 13:08:33 +0530

    create post functionality added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,29 +3,15 @@ import os
 from PIL import Image
 from flask import render_template,url_for,flash,redirect,request
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import RegistraionForm,LoginForm,UpdateAccountForm
+from flaskblog.forms import RegistraionForm,LoginForm,UpdateAccountForm,PostForm
 from flaskblog.models import User,Post
 from flask_login import login_user,current_user,logout_user,login_required
 
 
-posts = [
-    {
-        'author':'Corey Schafar',
-        'title':'Blog post 1',
-        'content':'first post content',
-        'date_posted':'April 20, 2020'
-    },
-    {
-        'author':'Hoa Doe',
-        'title':'Blog post 2',
-        'content':'second post content',
-        'date_posted':'April 23, 2020'
-    }
-]
-
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
@@ -104,3 +90,25 @@ def account():
 
     image_file = url_for('static',filename='profile_pics/' + current_user.image_file)
     return render_template('account.html',title="Account",image_file=image_file, form=form)
+
+
+@app.route('/post/new',methods=['POST','GET'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post  = Post( title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created !','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html',title="New Post", form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post  = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post=post)
+
+
+
